7d5c9aabad4ddcbcd95d0eda0fee7aa4a2e71bca
LostSol <159659123+TheCosmicSlug2@users.noreply.github.com>
2024-09-02 23:08:57 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ dic_colors = {0: None, 1: BLACK, 2: WHITE2, 3: RED, 4: BLUE, 5: GREEN}
 
 DEFAULT_SCREEN_DIMS = (800, 600)
 
-grid_dims = (20, 20)
+grid_dims = (100, 100)
 
 
 SCREEN_DIMS = DEFAULT_SCREEN_DIMS
@@ -35,7 +35,7 @@ command_input = ""
 PLAYER_SPAWN_TYPE = "random"
 player_side_size = max(1, (average_cell_size // 3))
 PLAYER_DIMS = (player_side_size, player_side_size)
-PLAYER_SPEED = average_cell_size // 10
+PLAYER_SPEED = max(1, average_cell_size / 10)
 
 
 RAY_DIMS = (3, 3)
@@ -49,6 +49,7 @@ RAYCASTER_GAP = 1
 
 # FOV_MAX pour calculer la FOV
 FOV_MAX = 60  # Plus réaliste pour réduire l'effet fisheye
+HALF_FOV = FOV_MAX // 2
 
 FPS = 30
 
